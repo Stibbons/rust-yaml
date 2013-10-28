91781de9de9dd03a952974b7196ceb9c18733057
Gaetan Semet <gaetan@xeberon.net>
2013-10-29 00:16:16 +0100

    imported project
    Signed-off-by: Gaetan Semet <gaetan@xeberon.net>

---
+++ README.md
@@ -0,0 +1,4 @@
+# The YAML Library for The Rust Programming Language
+
+This is a library for date and time for Rust
+
---
+++ src/libyaml/lib.rs
@@ -0,0 +1,9 @@
+#[link(name = "yaml",
+       vers = "0.1-pre",
+       url = "")];
+
+#[comment = "Rust YAML coder and decoder"];
+#[license = "Apache License"];
+#[crate_type = "lib"];
+
+pub mod parser;
